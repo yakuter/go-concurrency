995a3382d036dd518ad3a259493e4467350805f4
Erhan Yakut <yakuter@gmail.com>
2020-12-04 16:51:18 +0300

    05-done-channel

---
+++ 05-done-channel/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func hello(done chan bool) {
+	fmt.Println("Hello world goroutine")
+	done <- true
+}
+func main() {
+
+	// We can use channels to wait for goroutines
+	// This is called done channel which is in fact normal channel
+
+	done := make(chan bool)
+	go hello(done)
+
+	<-done
+	fmt.Println("main function")
+}
